382cb5d3ff46687f4419c8588a6ce77c8d7129db
Ivar Clemens <post@ivarclemens.nl>
2016-02-11 17:01:49 +0000

    Added hidden field

--- BackendXApp/models/database.js
+++ BackendXApp/models/database.js
@@ -15,16 +15,18 @@ mongoose.model('Experiment', experimentSchema);
 
 var participantSchema = new mongoose.Schema({
     //'password': String,
-    
+
     'experiment': [{ type: mongoose.Schema.Types.ObjectId, ref: 'Experiment' }],   
  
     'started': Date,
     'finished': Date,
+    
+    'hidden': Boolean,
 
     'responses': [{
         'timestamp': Date,
         'field': String,
-        'value': String
+        'value': mongoose.Schema.Types.Mixed
     }],
 });
 
