6a539cb0aa6aedd2015f6dd519755163898aee65
Ivar Clemens <post@ivarclemens.nl>
2016-02-12 12:17:44 +0000

    Renamed modules.

--- BackendXApp/public/js/app.js
+++ BackendXApp/public/js/app.js
@@ -1,43 +1,46 @@
-﻿
-var experimentBackendApp = angular.module('experimentBackendApp', [
-    'ngRoute',
-    'ngResource',
-    'experimentBackendControllers',
-    'menuControllers'
-]);
+﻿(function() {
+    
+    var module = angular.module('experimentFrameworkApp', [
+        'ngRoute',
+        'ngResource',
+        'experimentFrameworkControllers',
+        'menuControllers'
+    ]);
 
 
-experimentBackendApp.factory('Experiment', ['$resource', function ($resource) {
-    return $resource('experiment/:id', { id: "@_id" })
-}]);
+    module.factory('Experiment', ['$resource', function ($resource) {
+        return $resource('experiment/:id', { id: "@_id" })
+    }]);
 
 
-experimentBackendApp.factory('Participant', ['$resource', function ($resource) {    
-    return $resource('experiment/:experiment/participant/:id', { id: "@_id", experiment: "@experiment" });
-}]);
+    module.factory('Participant', ['$resource', function ($resource) {    
+        return $resource('experiment/:experiment/participant/:id', { id: "@_id", experiment: "@experiment" });
+    }]);
 
 
-experimentBackendApp.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.
-            when('/', {
-                redirectTo: '/experiments'
-        }).
-            when('/experiments', {
-                templateUrl: 'partials/experiment-list.html',
-                controller: 'ExperimentListController'
-        }).
-            when('/experiments/:experimentId', {
-                templateUrl: 'partials/experiment-details.html',
-                controller: 'ExperimentDetailController'
-        }).
-            when('/experiments/:experimentId/participants/invite', {
-                templateUrl: 'partials/participant-invite.html',
-                controller: 'ParticipantInviteController'
-        }).
-        	when('/experiment/:experimentId/participants/:participantId', {
-        		templateUrl: 'partials/participant-details.html',
-        		controller: 'ParticipantDetailController'
-        });
-    }
-]);
+    module.config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    redirectTo: '/experiments'
+            }).
+                when('/experiments', {
+                    templateUrl: 'partials/experiment-list.html',
+                    controller: 'ExperimentListController'
+            }).
+                when('/experiments/:experimentId', {
+                    templateUrl: 'partials/experiment-details.html',
+                    controller: 'ExperimentDetailController'
+            }).
+                when('/experiments/:experimentId/participants/invite', {
+                    templateUrl: 'partials/participant-invite.html',
+                    controller: 'ParticipantInviteController'
+            }).
+                when('/experiment/:experimentId/participants/:participantId', {
+                    templateUrl: 'partials/participant-details.html',
+                    controller: 'ParticipantDetailController'
+            });
+        }
+    ]);
+
+}());
--- BackendXApp/public/js/controllers.js
+++ BackendXApp/public/js/controllers.js
@@ -1,134 +1,136 @@
+(function() {
+       
+    var module = angular.module('experimentFrameworkControllers', []);
 
-var experimentBackendControllers = angular.module('experimentBackendControllers', []);
+    module.controller('ExperimentListController', ['$scope', '$location', 'Experiment',
+        function ($scope, $location, Experiment) {
+            $scope.experiment = {};
+            $scope.experiments = Experiment.query();
+            
+            $scope.add_experiment = function (exp) {
+                var experiment = new Experiment($scope.experiment);
 
-
-experimentBackendControllers.controller('ExperimentListController', ['$scope', '$location', 'Experiment',
-    function ($scope, $location, Experiment) {
-        $scope.experiment = {};
-        $scope.experiments = Experiment.query();
-        
-        $scope.add_experiment = function (exp) {
-            var experiment = new Experiment($scope.experiment);
-
-            experiment.$save(function () {
-                $scope.experiments.push(experiment);
-                $location.path('/experiments/' + experiment._id);
-            });
-        }
-    }
-]);
-
-
-experimentBackendControllers.controller('ExperimentDetailController', ['$scope', '$location', '$routeParams', 'Experiment',
-    function ($scope, $location, $routeParams, Experiment) {
-        Experiment.get(
-            { id: $routeParams.experimentId }, 
-            function (experiment) { 
-                $scope.experiment = experiment;
+                experiment.$save(function () {
+                    $scope.experiments.push(experiment);
+                    $location.path('/experiments/' + experiment._id);
+                });
             }
-        );
-        
-        $scope.delete_experiment = function (experiment) {
-            experiment.$delete(function () {
-                $location.path('/experiments');
-            });
-        };
-
-        $scope.update_experiment = function (experiment) {
-            experiment.$save();
-        };
-    }
-]);
+        }
+    ]);
 
 
-/**
- * Displays a list of participants
- */
-experimentBackendControllers.controller('ParticipantListController', ['$scope', '$location', '$routeParams', 'Experiment', 'Participant',
-    function ($scope, $location, $routeParams, Experiment, Participant) {
-        
-        /**
-         * Update list of participants, and find participant ID in responses
-         */
-        $scope.updateParticipantList = function()
-        {        
-            Participant.query({ 'experiment': $routeParams.experimentId }, function (participants) {
-                for(var i = 0; i < participants.length; i++) {
-                    
-                    for(var j = 0; j < participants[i].responses.length; j++) {
-                        var response = participants[i].responses[j];
+    module.controller('ExperimentDetailController', ['$scope', '$location', '$routeParams', 'Experiment',
+        function ($scope, $location, $routeParams, Experiment) {
+            Experiment.get(
+                { id: $routeParams.experimentId }, 
+                function (experiment) { 
+                    $scope.experiment = experiment;
+                }
+            );
+            
+            $scope.delete_experiment = function (experiment) {
+                experiment.$delete(function () {
+                    $location.path('/experiments');
+                });
+            };
+
+            $scope.update_experiment = function (experiment) {
+                experiment.$save();
+            };
+        }
+    ]);
+
+
+    /**
+     * Displays a list of participants
+     */
+    module.controller('ParticipantListController', ['$scope', '$location', '$routeParams', 'Experiment', 'Participant',
+        function ($scope, $location, $routeParams, Experiment, Participant) {
+            
+            /**
+             * Update list of participants, and find participant ID in responses
+             */
+            $scope.updateParticipantList = function()
+            {        
+                Participant.query({ 'experiment': $routeParams.experimentId }, function (participants) {
+                    for(var i = 0; i < participants.length; i++) {
                         
-                        if(response.field == "ParticipantID")
-                            participants[i].participantID = response.value;
+                        for(var j = 0; j < participants[i].responses.length; j++) {
+                            var response = participants[i].responses[j];
+                            
+                            if(response.field == "ParticipantID")
+                                participants[i].participantID = response.value;
+                        }
                     }
+                    
+                    $scope.participants = participants;
+                });
+            }
+
+            /**
+             * Create a record for a new participant
+             */
+            $scope.add_participant = function (experiment) {
+                var participant = new Participant({ 'experiment': experiment._id });
+                
+                participant.$save(function () {
+                    $scope.participants.push(participant);
+                });
+            };
+
+
+            /**
+             * Remove a participant from the list, if data is associated it will only be hidden 
+             */
+            $scope.delete_participant = function (participant) {
+                participant.$delete(function () {
+                    $scope.updateParticipantList();
+                });
+            };   
+            
+            
+            /**
+             * Populate list at when controller starts
+             */
+            $scope.updateParticipantList();    
+        }
+    ]);
+
+
+    module.controller('ParticipantDetailController', ['$scope', '$location', '$routeParams', 'Participant',
+        function($scope, $location, $routeParams, Participant) {
+            Participant.get({
+                id : $routeParams.participantId,
+                experiment: $routeParams.experimentId,
+            }, function(participant) {
+                $scope.participant = participant;
+                
+                var responses = {};
+                
+                for(var i = 0; i < participant.responses.length; i++) {
+                    var response = participant.responses[i];
+
+                    if(!(response.field in responses))
+                        responses[response.field] = [];
+                    
+                    responses[ response.field ].push(response);				
                 }
                 
-                $scope.participants = participants;
+                $scope.responses = responses;
             });
+
+        } 
+    ]);
+
+
+    module.controller('ParticipantInviteController', ['$scope', '$location', '$routeParams', 'Experiment', 'Participant',
+        function ($scope, $location, $routeParams, Experiment, Participant) {
+            Experiment.get(
+                { id: $routeParams.experimentId },
+                function (experiment) {
+                    $scope.experiment = experiment;
+                });
         }
+    ]);
 
-        /**
-         * Create a record for a new participant
-         */
-        $scope.add_participant = function (experiment) {
-            var participant = new Participant({ 'experiment': experiment._id });
-            
-            participant.$save(function () {
-                $scope.participants.push(participant);
-            });
-        };
-
-
-        /**
-         * Remove a participant from the list, if data is associated it will only be hidden 
-         */
-        $scope.delete_participant = function (participant) {
-            participant.$delete(function () {
-                $scope.updateParticipantList();
-            });
-        };   
-        
-        
-        /**
-         * Populate list at when controller starts
-         */
-        $scope.updateParticipantList();    
-    }
-]);
-
-
-experimentBackendControllers.controller('ParticipantDetailController', ['$scope', '$location', '$routeParams', 'Participant',
-    function($scope, $location, $routeParams, Participant) {
-		Participant.get({
-			id : $routeParams.participantId,
-            experiment: $routeParams.experimentId,
-		}, function(participant) {
-			$scope.participant = participant;
-			
-			var responses = {};
-			
-			for(var i = 0; i < participant.responses.length; i++) {
-				var response = participant.responses[i];
-
-				if(!(response.field in responses))
-					responses[response.field] = [];
-				
-				responses[ response.field ].push(response);				
-			}
-			
-			$scope.responses = responses;
-		});
-
-	} 
-]);
-
-
-experimentBackendControllers.controller('ParticipantInviteController', ['$scope', '$location', '$routeParams', 'Experiment', 'Participant',
-    function ($scope, $location, $routeParams, Experiment, Participant) {
-        Experiment.get(
-            { id: $routeParams.experimentId },
-            function (experiment) {
-                $scope.experiment = experiment;
-            });
-    }
-]);
+}());
